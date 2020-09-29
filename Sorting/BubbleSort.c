1bd13997b25cac14ed34e4eca060170902a46ae0
Mani5871 <58336312+Mani5871@users.noreply.github.com>
2020-09-29 19:20:55 +0530

    Update and rename 1.c to BubbleSort.c

--- Sorting/BubbleSort.c
+++ Sorting/BubbleSort.c
@@ -9,13 +9,13 @@ main()
         for(i=0;i<n;i++)
                 scanf("%d",&a[i]);
         for(i=0;i<n-1;i++)
-        for(j=0;j<n-i-1;j++)
-        if(a[j]>a[j+1])
-        {
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-        }
+                for(j=0;j<n-i-1;j++)
+                        if(a[j]>a[j+1])
+                        {
+                                temp=a[j];
+                                a[j]=a[j+1];
+                                a[j+1]=temp;
+                        }
         printf("The elements after sorting\n");
         for(i=0;i<n;i++)
                printf("%d ",a[i]);
